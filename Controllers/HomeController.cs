f5bd5835067a35fff81f82019b0324cdfc48bb9c
Steven <steven.chung96@yahoo.com>
2019-08-11 12:49:53 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dojo_survey_validations.Models;
+
+namespace dojo_survey_validations.Controllers
+{
+    public class HomeController : Controller   //remember inheritance??
+    {
+        //for each route this controller is to handle:
+        [HttpGet]       //type of request
+        [Route("")]     //associated route string (exclude the leading /)
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("method")]
+        public IActionResult Method(Survey survey)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Result", survey);
+            }
+            return View("Index");
+        }
+    }
+}
